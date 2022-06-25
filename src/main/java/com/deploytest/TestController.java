6c86d42a3888c54bb75dc51511eb44e944072ee3
ja04454 <ja04454@gmail.com>
2022-06-25 17:16:55 +0900

    update

---
+++ src/main/java/com/deploytest/TestController.java
@@ -0,0 +1,14 @@
+package com.deploytest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        String testStr = "Hi~~";
+        System.out.println(testStr);
+        return testStr;
+    }
+}
